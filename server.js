6c32ac895d44a51691808b2255d306b3fab92d8d
CJBoCo <websafe@cjboco.com>
2021-04-19 10:35:06 -0700

    Updated dynamic routing on server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+
 var express = require( 'express' );
 var http = require( 'http' );
 var path = require( 'path' );
@@ -14,15 +15,33 @@ app.use( logger( 'dev' ) );
 
 // my dist dir struture
 app.use( '/vendor', express.static( path.join( distDir, 'assets/js/vendor' ) ) );
-app.use( '/mylibs', express.static( path.join( distDir, 'assets/js/mylibs' ) ) );
+app.use( '/cjboco', express.static( path.join( distDir, 'assets/js/cjboco' ) ) );
 app.use( '/js', express.static( path.join( distDir, 'assets/js' ) ) );
 app.use( '/images', express.static( path.join( distDir, 'assets/images' ) ) );
 app.use( '/css', express.static( path.join( distDir, 'assets/css' ) ) );
 app.use( '/assets', express.static( path.join( distDir, 'assets' ) ) );
 
-app.get( '/', function ( req, res ) {
-	res.sendFile( path.join( distDir, 'index.html' ) );
+app.use('/**', (req, res) => {
+	/*
+	console.log(req.params); 		// "{ '0': 'about.html', '1': '' }"
+	console.log(req.protocol)     	// "https"
+	console.log(req.hostname)     	// "example.com"
+	console.log(req.path)         	// "/creatures"
+	console.log(req.originalUrl)  	// "/creatures?filter=sharks"
+	console.log(req.subdomains)   	// "['ocean']"
+	*/
+	if (req.originalUrl.length > 1) {
+		res.sendFile( path.join( distDir, req.originalUrl.slice(1) ) );
+	} else {
+		res.sendFile( path.join( distDir, '/' ) );
+	}
+
 } );
+
+
+app.get('/', (req, res) => {
+	res.sendFile( path.join( distDir, 'index.html' ) );
+});
 
 var server = http.createServer( app );
 
